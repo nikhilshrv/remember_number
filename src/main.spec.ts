3b67d838e0a895fd0a425073083be5016e50a841
Nikhil Shrivastav <nikhil.shrivastav@votercircle.com>
2020-12-21 12:43:39 +0545

    Added test for checking if a number exists

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -5,3 +5,9 @@ const rememberNumber = new RememberNumber();
 test("should be able to remember a number", () => {
     expect(rememberNumber.rememberNumber(3)).toBe(true);
 });
+
+test("should be able to check if a number exists", () => {
+    expect(rememberNumber.checkIfNumberExists(34756)).toBe(false);
+    rememberNumber.rememberNumber(34756);
+    expect(rememberNumber.checkIfNumberExists(34756)).toBe(true);
+});
