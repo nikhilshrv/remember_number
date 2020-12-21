b6e51f8b7517a72bd165ade6bfe172194b2e5d15
Nikhil Shrivastav <nikhil.shrivastav@votercircle.com>
2020-12-21 12:44:55 +0545

    Added test for forgetting a number

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -11,3 +11,12 @@ test("should be able to check if a number exists", () => {
     rememberNumber.rememberNumber(34756);
     expect(rememberNumber.checkIfNumberExists(34756)).toBe(true);
 });
+
+test("should be able to forget a number", () => {
+    expect(rememberNumber.checkIfNumberExists(400001)).toBe(false);
+    rememberNumber.rememberNumber(400001);
+    expect(rememberNumber.checkIfNumberExists(400001)).toBe(true);
+
+    rememberNumber.forgetNumber(400001);
+    expect(rememberNumber.checkIfNumberExists(400001)).toBe(false);
+});
